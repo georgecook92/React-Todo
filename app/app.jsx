06c724daf77d3b6cec13fe59f6a8086700f0ec47
George <george.cook.92@hotmail.co.uk>
2016-06-11 16:46:23 +0100

    initial commit - adding basic todoapp component

--- app/app.jsx
+++ app/app.jsx
@@ -3,6 +3,8 @@ var ReactDOM = require('react-dom');
 //destructuring syntax - same as - var Route = require('react-router').Route and so on
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
+var TodoApp = require('TodoApp');
+
 //load foundation-sites - uses loaders also
 $(document).foundation();
 
@@ -11,6 +13,6 @@ require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
   //router component
-  <p>Boilerplate 3 project</p>,
+  <TodoApp />,
   document.getElementById('app')
 );
