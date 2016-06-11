40d4086eb255f84fbfb3fe966ea3bb122b260dd6
George <george.cook.92@hotmail.co.uk>
2016-06-11 17:13:42 +0100

    adding basic list functionality

---
+++ app/components/TodoList.jsx
@@ -0,0 +1,30 @@
+var React = require('react');
+var Todo = require('Todo');
+
+var TodoList = React.createClass({
+
+  render: function() {
+    var {todos} = this.props;
+
+    var renderTodos = () => {
+      //function gets called for each item in
+      return todos.map( (todo) => {
+        //have to have a key property so react can keep a track
+        //... spread operator - combined with destructuring - creates props for all properties of todo
+        return (
+          <Todo key={todo.id} {...todo} />
+        );
+      } );
+    };
+
+    return (
+      <div>
+        {renderTodos()}
+      </div>
+    )
+
+  }
+
+});
+
+module.exports = TodoList;
