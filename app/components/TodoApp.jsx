b57a30912ed16dec8318d054f31ff6eb5954c6f7
George <george.cook.92@hotmail.co.uk>
2016-06-13 11:18:15 +0100

    add TodoAPI with localstorage plus tests

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,8 +1,10 @@
 var React = require('react');
+var uuid = require('node-uuid');
+
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
 var TodoSearch = require('TodoSearch');
-var uuid = require('node-uuid');
+var TodoAPI = require('TodoAPI');
 
 var TodoApp = React.createClass({
 
@@ -10,30 +12,12 @@ var TodoApp = React.createClass({
     return {
       showCompleted: false,
       searchText: '',
-      todos: [
-        {
-          id: uuid(),
-          text: 'Walk the dog.',
-          completed: false
-        },
-        {
-          id: uuid(),
-          text: 'Clean the garden',
-          completed: true
-        },
-        {
-          id: uuid(),
-          text: 'Learn React',
-          completed: true
-        },
-        {
-          id: uuid(),
-          text: 'Get a job',
-          completed: false
-        }
+      todos: TodoAPI.getTodos()
+    };
+  },
 
-      ]
-    }
+  componentDidUpdate: function() {
+    TodoAPI.setTodos(this.state.todos);
   },
 
   handleAddTodo: function(text) {
